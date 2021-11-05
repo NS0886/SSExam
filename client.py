18a4448b33d5a56b0cfe28a53577f6682e92e0d9
NS0886 <93697745+NS0886@users.noreply.github.com>
2021-11-04 21:28:10 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+import socket
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+socket.bind((socket.gethostname(), 8000))
+socket.listen() #create a queue of 5 
+
+    while True:
+        clientsocket, address = socket.accept()
+        inputfunction = raw_input("Enter add(), delete(), search(), autocomplete(), or display(): ");
+        clientsocket.send(inputfunction.encode()); #send code to server to input in trie
+        fromserver = clientsocket.recv(1024)
+        print(fromserver.decode());  #print output of trie based on user input
+        clientsocket.close() #close connection
 
